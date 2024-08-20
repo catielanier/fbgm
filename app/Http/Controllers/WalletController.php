77f7a359cb0bc28fac66b4630dcc8b7cdf11188f
Catie Lanier <hello@catielanier.ca>
2024-08-20 19:37:26 -0400

    add wallet deletion route

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -90,4 +90,30 @@ class WalletController extends Controller {
         $updated = $wallet->update($validated);
         return response()->json($updated, 200);
     }
+
+    public function destroy(Request $request, $id) {
+        $token = null;
+        $headers = $request->header();
+        if (isset($headers['Authorization'])) {
+            $authorization = $headers['Authorization'];
+            $matches = [];
+            preg_match('/Bearer\s(\S+)/', $authorization, $matches);
+            if (isset($matches[1])) {
+                $token = $matches[1];
+            } else {
+                return response()->json(['error' => 'Unauthorized'], 403);
+            }
+        } else {
+            return response()->json(['error' => 'Authorization header not found.'], 403);
+        }
+        $jwtSecret = env('SECRET');
+        $decoded = JWT::decode($token, new Key($jwtSecret, 'HS256'));
+        $tokenArr = (array)$decoded->data;
+        $result = Wallet::where('_id', $id)->first();
+        if ($result->user !== $tokenArr['userId']) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+        $result->delete();
+        return response()->json(null, 204);
+    }
 }
